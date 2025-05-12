0b7cf913c4d547aca04315e1c37267bf03dc98e9
Joseph Macharia <37827513+webmaster254@users.noreply.github.com>
2025-05-12 22:14:18 +0300

    Create schema.sql

---
+++ database-script-0x01/schema.sql
@@ -0,0 +1,71 @@
+-- User Table
+CREATE TABLE User (
+    user_id UUID PRIMARY KEY,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    email VARCHAR UNIQUE NOT NULL,
+    password_hash VARCHAR NOT NULL,
+    phone_number VARCHAR,
+    role ENUM('guest', 'host', 'admin') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Property Table
+CREATE TABLE Property (
+    property_id UUID PRIMARY KEY,
+    host_id UUID NOT NULL,
+    name VARCHAR NOT NULL,
+    description TEXT NOT NULL,
+    location VARCHAR NOT NULL,
+    pricepernight DECIMAL NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (host_id) REFERENCES User(user_id)
+);
+
+-- Booking Table
+CREATE TABLE Booking (
+    booking_id UUID PRIMARY KEY,
+    property_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL NOT NULL,
+    status ENUM('pending', 'confirmed', 'canceled') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (property_id) REFERENCES Property(property_id),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+-- Payment Table
+CREATE TABLE Payment (
+    payment_id UUID PRIMARY KEY,
+    booking_id UUID NOT NULL,
+    amount DECIMAL NOT NULL,
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    payment_method ENUM('credit_card', 'paypal', 'stripe') NOT NULL,
+    FOREIGN KEY (booking_id) REFERENCES Booking(booking_id)
+);
+
+-- Review Table
+CREATE TABLE Review (
+    review_id UUID PRIMARY KEY,
+    property_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    rating INTEGER CHECK (rating >= 1 AND rating <= 5) NOT NULL,
+    comment TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (property_id) REFERENCES Property(property_id),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+-- Message Table
+CREATE TABLE Message (
+    message_id UUID PRIMARY KEY,
+    sender_id UUID NOT NULL,
+    recipient_id UUID NOT NULL,
+    message_body TEXT NOT NULL,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES User(user_id),
+    FOREIGN KEY (recipient_id) REFERENCES User(user_id)
+);
